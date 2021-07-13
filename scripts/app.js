081038208ba95b34c5ee11513168c453f72372e9
geearen <allengonzales018@gmail.com>
2021-07-12 22:55:06 -0700

    Created the USER STORIES/ GAME LOGIC

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,54 @@
 console.log("JUJUTSU KAISEN");
+
+/*  ==== MVP ==== */
+// KEEP IT MINIMUM and FUNCTIONING then add feature later
+
+//===== SECTION ====
+/*  USER STORIES/ GAME LOGIC */ 
+
+/* NOTE */
+// In the beginning of the game user will be prompted and ask for their the nickname they want to use TODO
+// Main Character of the game will Yuji Itadori, the objective of the game is Role - Play as him 
+// Itadori has three actions (buttons) it will be FOOD, TRAIN and SLEEP. 
+
+//FOOD --> will increases Itadori health and lower his hunger level
+//TRAIN --> will increase Itadori health by a little and increase his hunger level as well
+//SLEEP ---> will lower his sleepiness level and increase his health level
+
+/* NOTE */
+//As a player/User there's three metrics to keep an eye on for: Health, Hunger and Sleepiness
+/*  SECTION */
+//Health --> Maintaning his health bar will keep the Itadori alive ---> Losing health to empty health bar = GAMEOVER
+//Hunger ---> As hunger increases Itadori's health deplete slowly ---> Losing health = GAME OVER
+//Sleepiness ----> As sleepiness increases and reaching max -----> will also affect his Health
+
+/* NOTE */
+// For random period of time monster/foe/enemy will spawn and decrease his health and hunger
+// User has to the ability to use FOOD,SLEEP and TRAIN to increase his health or hunger
+
+
+/* NOTE */
+// As the game progress, user will be prompted to level up and Itadori will eat the SAKUNA's finger.
+// SUKUNA finger acts as a level or round counter === max level is 20 (20 fingers)=== once it reach the GAME IS OVER YOU WIN!
+
+
+
+
+
+
+/* REVIEW */
+//EXTRA FEATURE ===== Feature Creep ======= ICE BOX ======== STRETCH GOALS
+/* 
+    * add a power lvl metric that act as a (the level of the game / the higher the number the harder it gets to keep Itadori alive)
+    * add a SAKUNA METRIC  ( experience level)
+    * add a confidence boost that will boost up the SAKUNA BOOST = each SAKUNA BOOST INCREASES the POWER LEVEL === the faster the game progress
+    * add more monster having differently damage level , also increasing the SAKUNA BOOST == faster the game progress
+    * add sounds
+    * add animations when button is being press
+    * 
+
+
+
+
+
+*/
