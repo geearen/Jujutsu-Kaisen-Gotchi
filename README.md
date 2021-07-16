08c9b274d25d972daf09f0d0da47811918867828
geearen <allengonzales018@gmail.com>
2021-07-15 22:30:17 -0700

    added some finishing touch

--- README.md
+++ README.md
@@ -5,13 +5,16 @@
 
 ![KAISEN](https://occ-0-1723-1722.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABenF3_UnaazjvWtxJrkgO66DOTKiMqaHO4kWi1Hs5T2HmCtjwVopytB2Vk9mdqe_IitU3BwvUSsMmxckw9BdVUtNmJQQt9e0RBqV.png?r=1e9)
 
-# Objective
-An interactive game where player will play as Itadori Yuji from the show called Jujutsu Kaisen. The objective of the game is to survive and reach Sukuna Level 20 to win the game! To survive, player has to keep an eye on the metric bar / progress bar and use the FOOD, TRAIN and SLEEP to keep Itadori Yuji alive!
+# Why?
+I picked Jujutsu Kaisen as my theme for my Tamagotchi game because I thought it would be fun to create a game out of it and I really enjoyed watching the show. I tried to mimic the game logic of a Tamagotchi.
+
+# Game Objective
+An interactive game where player will play as Itadori Yuji from the show called Jujutsu Kaisen. The objective of the game is to survive and reach Sukuna Level 6 to win the game! To survive, player has to keep an eye on the metric bar / progress bar and use the FOOD, TRAIN and SLEEP to keep Itadori Yuji alive!
 
 # User Stories
 1.  In the beginning of the game, user/player will be ask for the nickname they want to use for the game. Before starting the game, Itadori will start with Health stats of 8 out of 10, Hunger stats of 2 out 10, Sleepiness stats of 1 out 10 and Sukuna level of 0. 
 
-2. Player has to press the start button to begin the game. Once pressed, TIMER will start from 0s and reaching to 60s. Reaching the 60s will increment the sukuna level by 1 === every 20s is equal to +1 Sukuna. Reaching 20 Sukuna, the player wins the game! or beat the game.
+2. Player has to press the start button to begin the game. Once pressed, TIMER will start from 0s and reaching to 60s. Reaching the 10s will increment the sukuna level by 1 === every 10s is equal to +1 Sukuna. Reaching 6 Sukuna, the player wins the game! or beat the game.
 
 3. Things to keep an eye for is the Health, Hunger and Sleepiness stats. Once the game begins, Each metric will fluctuate respective to time. Hunger will increase  over time , once it reaches 10/10 Hunger - Health will decrease by 1. Same thing goes for sleepiness, reaching 10/10 Sleepiness - Health decrease by 1.  Unable to keep the Health bar above 0 will result to GAME OVER --- Itadori dies 😔.
 
@@ -26,6 +29,8 @@ An interactive game where player will play as Itadori Yuji from the show called 
 # WireFrame 
 ![WIREFRAME](https://i.imgur.com/cN9tW1w.png)
 
+# Technologies 
+JavaScript - HTML - CSS - Animate.CSS - NES CSS Framework 
 # Milestone
 
     1. Set-up the html,css, and javascript file. Added the basic css and index.html ✔️DONE
@@ -45,18 +50,19 @@ An interactive game where player will play as Itadori Yuji from the show called 
     15. Add a naming function and hide inputs after completed ✔️ DONE
     16. Add animation ✔️  DONE
     17. Reviewed the game logic and fixed bugs/problem ✔️ DONE
-    18. Add winning animation 
+    18. Add winning and losing display ✔️ DONE
+    19. Add background sound ✔️ DONE
 
 # Feature Creep
-    * Food button ----> decrease hunger by 1
+    * Food button ----> decrease hunger by 6 ADDED
     * Train button -----> and increase sleepiness by 1
     * Sleep button --->Hunger will increase by 1 
     * add a power lvl metric that act as a (the level of the game / the higher the number the harder it gets to keep Itadori alive)
     * add a confidence boost that will boost up the SAKUNA BOOST = each SAKUNA BOOST INCREASES the POWER LEVEL === the faster the game progress
     * add more monster having differently damage level , also increasing the SAKUNA BOOST == faster the game progress
-    * add sounds
-    * add animations when button is being press DONE
-    * disables other buttons, one button at a time and no repeating button DONE
+    * add sounds ADDED
+    * add animations when button is being press ADDED 
+    * disables other buttons, one button at a time and no repeating button ADDED
 
 # Resources
     * https://www.animecharactersdatabase.com/characters.php?id=104841
@@ -68,3 +74,6 @@ An interactive game where player will play as Itadori Yuji from the show called 
     * https://www.deviantart.com/shinasty/art/Anime-bedroom-Backround-782719886
     * https://recenthighlights.com/jujutsu-kaisen-episode-6-release-date/
     * https://www.zerochan.net/3232404
+    * https://www.jujutsukaisen.jp/character/
+    * https://www.deviantart.com/rdxscythe/art/Itadori-Yuji-874618518
+    * https://soundcloud.com/kyzuu/jujutsu-kaisen-ending-lost-in-paradise-feat-aklo-slowed-and-reverb
--- scripts/app.js
+++ scripts/app.js
@@ -1,83 +1,5 @@
 console.log("JUJUTSU KAISEN");
 
-/*  ==== MVP ==== */
-// KEEP IT MINIMUM and FUNCTIONING then add feature later
-
-//===== SECTION ====
-/*  USER STORIES/ GAME LOGIC */ 
-
-/* NOTE */
-// In the beginning of the game user will be prompted and ask for their the nickname they want to use TODO
-// Main Character of the game will Yuji Itadori, the objective of the game is Role - Play as him 
-// HEALTH STATS will start at 8 out 10 TODO DONE
-// HUNGER STATS will start at 2 out 10  TODO DONE
-// SLEEPINESS will start at 1 out 10  TODO DONE
-// SUKUNA bar will start at 0 it will increase over time (use of timer) and shows the round  ===== MAX LEVEL is 20. TODO DONE
-//TIME has max time of 120s
-
-
-
-/* NOTE */
-//For the player there's three metrics to keep an eye on for: Health, Hunger, Sleepiness and SUKUNA level/bar
-//There's a time interval where per 30sec Hunger and Sleepiness will increase by 1. TODO
-//Theres a time interval where per minute SUKUNA Level increase acts a round indication and morphing
-
-/*  SECTION */
-//Health ----> Maintaning his health bar will keep the Itadori alive ---> Losing health to empty health bar = GAMEOVER 
-/* Itadori will start the game with 8 health out of 10  ---> keeping it high enough will let survie the game*/
-
-//Hunger ----> As hunger increases Itadori's health deplete slowly ---> Losing health = GAME OVER
-/* Itadori hunger stats need to be keptlow if it reaches more than 8 out of 10 === Itadori health will decrease by 2 */
-
-//Sleepiness ----> As sleepiness increases and reaching max -----> will also affect his Health
-/* Itadori sleepiness stats need to be kept low when it reaches 10 or 10  ==== Itadori health will decrease by 1 */
-
-
-
-/* NOTE */
-// Itadori has three actions (buttons) it will be FOOD, TRAIN and SLEEP. 
-/* SECTION */
-//FOOD --> will increases Itadori health and lower his hunger level    TODO
-/*  FOOD button when clicked Health stats will increase by 2  */
-
-//TRAIN --> will increase Itadori health by a little and increase his hunger & sleepiness level as well TODO
-/* TRAIN button when clicked Health stats will increase by 1 and Increase hunger by 2 */
-
-//SLEEP ---> will lower his sleepiness level and increase his health level  TODO
-/* Sleep button will act as a rest --> Sleep will decrease by 2 */
-
-
-
-/* NOTE */
-// As the game progress, user will be prompted to level up and Itadori will eat the SAKUNA's finger.
-// SUKUNA finger acts as a level or round counter === max level is 20 (20 fingers)=== once it reach the GAME IS OVER YOU WIN!
-
-
-
-
-
-/* NOTE */
-/* REVIEW feature creep */  
-// For random period of time monster/foe/enemy will spawn and decrease his health and hunger
-// User has to the ability to use FOOD,SLEEP and TRAIN to increase his health or hunger
-
-
-
-/* REVIEW */
-//EXTRA FEATURE ===== Feature Creep ======= ICE BOX ======== STRETCH GOALS
-/* 
-    * Food button ---->decrease hunger by 1
-    * Train button -----> and increase sleepiness by 1
-    * Sleep button --->Hunger will increase by 1 
-    * add a power lvl metric that act as a (the level of the game / the higher the number the harder it gets to keep Itadori alive)
-    * add a SAKUNA METRIC  ( experience level)
-    * add a confidence boost that will boost up the SAKUNA BOOST = each SAKUNA BOOST INCREASES the POWER LEVEL === the faster the game progress
-    * add more monster having differently damage level , also increasing the SAKUNA BOOST == faster the game progress
-    * add sounds
-    * add animations when button is being press
-    * disables other buttons, one button at a time and no repeating button
-
-*/
 
 const kaisen = {
     nickname : "",
@@ -116,10 +38,11 @@ const kaisen = {
         $('#hunger').text(`Hunger:  ${kaisen.hunger}`);
         $('#sleepiness').text(`Sleepiness: ${kaisen.sleepiness}`);
         $('#timer__number').text(`${kaisen.time}s`);
+        $('#level').text(`Sukuna: ${kaisen.sukuna}`);
         $('.button.food').attr({ "disabled": false, "class": "button food nes-btn"});
         $('.button.train').attr({ "disabled": false, "class": "button train nes-btn" });
         $('.button.sleep').attr({ "disabled": false, "class": "button sleep nes-btn" });
-        $('.display__character').attr({"id": "animation__reset", "visibility": "visible"});
+        $('.display__character').attr({ "id": "animation__reset", "visibility": "visible", "src": "http://ami.animecharactersdatabase.com/uploads/chars/67712-2096367195.png"});
         $('.display__background').attr({"src": "https://i.pinimg.com/originals/09/5a/e8/095ae863a15f046100edf8efaa240452.jpg", style: "opacity:1"});
     },
 
@@ -234,10 +157,16 @@ const kaisen = {
     increaseSukunaLevel(){
         kaisen.sukuna++;
         $('#level').text(`Sukuna: ${kaisen.sukuna}`);
-        if(kaisen.sukuna > 6){
+        if(kaisen.sukuna >= 6){
             kaisen.winRound();
             clearInterval(kaisen.game);
             clearInterval(kaisen.timer);
+        }else if (kaisen.sukuna >= 4){
+            $('.display__character').attr("src", "https://www.jujutsukaisen.jp/images/chara_detail5.png");
+        }else if (kaisen.sukuna > 3){
+            $('.display__character').attr("src", "images/yuji.png");
+        }else if (kaisen.sukuna > 2){
+            $('.display__character').attr("src", "images/yuji2.png")
         }
         
     },
